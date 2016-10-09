f16782a5064171d9d9d2b8c638a270be7fe4497b
Giulio Moro <giuliomoro@yahoo.it>
2016-10-09 20:26:57 +0000

    Fixed sinfv_neon

--- math_sinfv.c
+++ math_sinfv.c
@@ -37,55 +37,6 @@ const float __sinfv_lut[4] = {
 	+0.99999661f,	//p1
 };
 
-#ifdef NOT
-float sinf_neon(float x)
-{
-	asm volatile (
-	
-	"vld1.32 		d3, [%0]				\n\t"	//d3 = {invrange, range}
-	"vdup.f32 		d0, d0[0]				\n\t"	//d0 = {x, x}
-	"vabs.f32 		d1, d0					\n\t"	//d1 = {ax, ax}
-	
-	"vmul.f32 		d2, d1, d3[0]			\n\t"	//d2 = d1 * d3[0] 
-	"vcvt.u32.f32 	d2, d2					\n\t"	//d2 = (int) d2
-	"vmov.i32	 	d5, #1					\n\t"	//d5 = 1	
-	"vcvt.f32.u32 	d4, d2					\n\t"	//d4 = (float) d2	
-	"vshr.u32 		d7, d2, #1				\n\t"	//d7 = d2 >> 1
-	"vmls.f32 		d1, d4, d3[1]			\n\t"	//d1 = d1 - d4 * d3[1]
-	
-	"vand.i32 		d5, d2, d5			\n\t"	//d5 = d2 & d5
-	"vclt.f32 		d18, d0, #0			\n\t"	//d18 = (d0 < 0.0)
-	"vcvt.f32.u32 	d6, d5				\n\t"	//d6 = (float) d5
-	"vmls.f32 		d1, d6, d3[1]		\n\t"	//d1 = d1 - d6 * d3[1]
-	"veor.i32 		d5, d5, d7			\n\t"	//d5 = d5 ^ d7	
-	"vmul.f32 		d2, d1, d1			\n\t"	//d2 = d1*d1 = {x^2, x^2}, xx = ax.f * ax.f
-	
-	"vld1.32 		{d16, d17}, [%1]	\n\t"	//q8 = {p7, p3, p5, p1}, __sinf_lut
-	"veor.i32 		d5, d5, d18			\n\t"	//d5 = d5 ^ d18, n = n ^ m	
-	"vshl.i32 		d5, d5, #31			\n\t"	//d5 = d5 << 31, n = n << 31
-	"veor.i32 		d1, d1, d5			\n\t"	//d1 = d1 ^ d5, ax.i = ax.i ^ n
-	
-	"vmul.f32 		d3, d2, d2			\n\t"	//d3 = d2*d2 = {x^4, x^4}, xxxx = xx*xx
-	"vmul.f32 		q0, q8, d1[0]		\n\t"	//q0 = q8 * d1[0] = {p7x, p3x, p5x, p1x}
-	"vmla.f32 		d1, d0, d2[0]		\n\t"	//d1 = d1 + d0*d2 = {p5x + p7x^3, p1x + p3x^3}		
-	"vmla.f32 		d1, d3, d1[0]		\n\t"	//d1 = d1 + d3*d0 = {...., p1x + p3x^3 + p5x^5 + p7x^7}		
-	: 
-	: "r"(__sinf_rng), "r"(__sinf_lut) 
-    : "q0", "q1", "q2", "q3", "q4", "q5", "q6", "q7","q8", "q9"
-	);
-}
-#endif
-	//n = n ^ m;
-	//m = (x < 0.0);
-	//n = n ^ m;	
-	//n = n << 31;
-	//ax.i = ax.i ^ n; 
-	//
-	//xx = ax.f * ax.f;	
-	//a = (__sinf_lut[0] * ax.f) * xx + (__sinf_lut[2] * ax.f);
-	//b = (__sinf_lut[1] * ax.f) * xx + (__sinf_lut[3] * ax.f);
-	//xx = xx * xx;
-	//r = b + a * xx;
 void sinfv_c(float *x, int n, float *r)
 {
 	union {
@@ -170,8 +121,7 @@ void sinfv_neon(float *x, int n, float *y)
 	"vmls.f32 		q1, q4, d14[1]	\n\t"	//q1 = q1 - q4 * d14[1]
 	
 	"vand.i32 		q5, q2, q5		\n\t"	//q5 = q2 & q5
-	"vmov.i32		q15, q15   \n\t" // q15 = 0
-	"vclt.f32 		q15, q0, q15	\n\t"	//q15 = (q0 < 0.0)
+	"vclt.f32 		q15, q0, #0	\n\t"	//q15 = (q0 < 0.0)
 	"vcvt.f32.u32 	q6, q5			\n\t"	//q6 = (float) q5
 	"vmls.f32 		q1, q6, d14[1]	\n\t"	//q1 = q1 - q6 * a23[1]
 	"veor.i32 		q5, q5, q8		\n\t"	//q5 = q5 ^ d7	
